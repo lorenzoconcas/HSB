de2fa9e679997c16190a45cdcaa168756f44b57c
Lorenzo Concas <35010999+lorenzoconcas@users.noreply.github.com>
2023-07-20 11:52:02 +0200

    Added examples
    Added first two examples (Hello World) that show how to do basic routing

---
+++ Examples/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using HSB;
+
+Configuration c = new()
+{
+    port = 8080, //you must be root to listen on port 80, so 8080 will be used instead (see http alternate port)
+    address = "" //with empty string the server will still listen to localhost but won't be available externally
+};
+
+//expressjs-like routing
+
+
+c.GET("/", (Request req, Response res) =>
+{
+    //reply to request with an hello world
+    res.SendHTMLContent("<h1>Hello world</h1>");
+});
+
+
+new Server(c).Start();
---
+++ Examples/HelloWorldServlet/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using HSB;
+
+namespace Example
+{
+    [Binding("/")] //route the root page
+    public class HelloWorld : Servlet
+    {
+        public HelloWorld(Request req, Response res) : base(req, res)
+        {
+        }
+
+        //we override the function that handle the GET response processing
+        public override void ProcessGet(Request req, Response res)
+        {
+            //reply to request with an hello world
+            res.SendHTMLContent("<h1>Hello world</h1>");
+        }
+
+
+        //same for the post request
+        public override void ProcessPost(Request req, Response res)
+        {
+            base.ProcessPost(req, res);
+            //if no change are made, the server will reply with code 405
+        }
+    }
+}

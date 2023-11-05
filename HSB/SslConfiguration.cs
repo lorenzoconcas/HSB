f7e941f65b367d0725727d94d733f3744579d245
Lorenzo Concas <35010999+lorenzoconcas@users.noreply.github.com>
2023-11-05 01:13:41 +0100

    Added ability to set certificate bytes

--- HSB/SslConfiguration.cs
+++ HSB/SslConfiguration.cs
@@ -16,6 +16,7 @@ public class SslConfiguration
     public bool serveOnlyWithSSL = false;
     public bool upgradeUnsecureRequests = true;
     public string? CertificatePath;
+    public byte[]? CertificateBytes;
     public string? CertificatePassword;
     public List<TLSVersion> TLSVersions;
     public bool CheckCertificateRevocation = true;
@@ -55,14 +56,52 @@ public class SslConfiguration
         ClientCertificateRequired = clientCertificateRequired;
     }
 
-    public bool ConfigIsValid() => enabled && CertificatePath != null && CertificatePassword != null && File.Exists(CertificatePath);
+    public bool ConfigIsValid() => enabled && (CertificatePath != null || CertificateBytes != null) && CertificatePassword != null && File.Exists(CertificatePath);
 
+
+    /// <summary>
+    /// Returns a X509Certificate2 object from the certificate path and password.
+    /// If a certificate in form of bytes is provided, it will be used instead of the path.
+    /// </summary>
+    /// <returns></returns>
     public X509Certificate2 GetCertificate()
     {
+        if (CertificateBytes != null)
+        {
+            return new(CertificateBytes, CertificatePassword!);
+        }
         return new(CertificatePath!, CertificatePassword!);
     }
-
-    public SslProtocols GetProtocols(){
+    /// <summary>
+    /// Sets the certificate path
+    /// </summary>
+    /// <param name="path"></param>
+    public void SetCertificate(string path)
+    {
+        CertificatePath = path;
+    }
+    /// <summary>
+    /// Sets the certificate data
+    /// </summary>
+    /// <param name="bytes"></param>
+    public void SetCertificate(byte[] bytes)
+    {
+        CertificateBytes = bytes;
+    }
+    /// <summary>
+    /// Sets the certificate password
+    /// </summary>
+    /// <param name="password"></param>
+    public void SetCertificatePassword(string password)
+    {
+        CertificatePassword = password;
+    }
+    /// <summary>
+    /// Returns the TLS protocols to use in a SSL Stream
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="DeprecatedTLSVersionException"></exception>
+    internal SslProtocols GetProtocols(){
         SslProtocols protocols = SslProtocols.None;
         if(TLSVersions.Count == 0){
            return SslProtocols.None;
